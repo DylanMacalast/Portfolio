82a462d701502bfe980f64661e38c58cad4fc0f1
DylanMacalast <dylanmacalast1@gmail.com>
2019-04-15 20:02:03 +0100

    page header

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,16 @@
-// drop down nav js code
+// NAV BAR
 export const navSlide = () => {
     const burger = document.querySelector('.navbar__burger');
     const navbar__ul = document.querySelector('.navbar__ul');
 
+    //toggle nav
     burger.addEventListener('click', ()=>{
         navbar__ul.classList.toggle('navbar--active');
-        
-    })
+
+         //burger animation
+        burger.classList.toggle('toggle');
+    });
+
+   ;
 }
+
