fecd1a70e6a438d8d6101aec49e18855b4db6794
DylanMacalast <dylanmacalast1@gmail.com>
2019-04-15 10:43:15 +0100

    setting up the file structure for project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 require('normalize.css/normalize.css'); // Resets all css
 require('./styles/index.scss'); // adding main scss to js so its compiled into js
+import PopperJS from 'popper.js';
+import jquery from 'jquery';
+
+
 
