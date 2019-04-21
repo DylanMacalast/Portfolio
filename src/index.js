7ca6d4ad0f536387a808fa7f66ffbffd3feb10a5
DylanMacalast <dylanmacalast1@gmail.com>
2019-04-21 17:03:45 +0100

    this is the skill block

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 require('normalize.css/normalize.css'); // Resets all css
 require('./styles/index.scss'); // adding main scss to js so its compiled into js
 import {navSlide} from './js/app';
-import PopperJS from 'popper.js';
 import jquery from 'jquery';
+import PopperJS from 'popper.js';
 
+jquery(()=>{
+    console.log('Hello jQuery + bootstrap 4!');
+})
 
 navSlide();
-
 
--- src/js/app.js
+++ src/js/app.js
@@ -14,3 +14,14 @@ export const navSlide = () => {
    ;
 }
 
+
+// SKILL BLOCK
+// get all elements with tab content class and hide them
+const tabContent = document.getElementsByClassName('tab__content');
+for (let i = 0; i < tabContent.length; i++) {
+    tabContent[i].style.display = 'none';
+}
+
+// get all elements with class="skills__nav--link" and remove class active
+
+
